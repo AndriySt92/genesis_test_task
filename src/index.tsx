a2a8aa5bd917da6727b984e22eb224799df69f84
AndriySt92 <andriyua-1992@mail.ru>
2023-03-19 17:23:40 +0200

    Configure React app for deployment to GitHub Pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
 import { Provider } from 'react-redux'
@@ -9,8 +9,8 @@ import { store } from './store'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
 )
